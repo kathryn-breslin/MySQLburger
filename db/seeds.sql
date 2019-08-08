587814acb5d0611333e65d563a324ddcdbc8ad5c
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-07 22:15:57 -0400

    add db folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger) VALUES ('Cheeseburger', 0);
+INSERT INTO burgers (burger) VALUES ('Veggie Burger', 0);
+INSERT INTO burgers (burger) VALUES ('Bison Burger', 0);
+INSERT INTO burgers (burger) VALUES ('Angus Burger', 0);
+
+
