0d53254f5e7da4f83300ce89418bd540204cd086
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-08 14:16:38 -0400

    select all controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,13 @@ var express = require('express');
 var burger = require('../models/burger.js');
 
 var router = express.Router();
+
+router.get('/', function(req, res){
+    burger.selectAll(function(data) {
+        var handlebarsObject = {
+            burgers: data
+        };
+        console.log(handlebarsObject);
+        res.render('index', handlebarsObject);
+    })
+})
