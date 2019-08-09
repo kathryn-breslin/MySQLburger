b2091013f902f0efaf05fc3a80c778d0759b0a8a
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-09 10:30:03 -0400

    put method to api

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,12 +14,29 @@ router.get('/', function(req, res){
 });
 
 router.post('/api/burgers', function(req, res) {
-    burger.create([
+    burger.insertOne([
         "burger_name", "devoured"
     ], [
         req.body.burger_name, req.body.devoured
         //check index for correct name value
     ], function(result) {
         res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("This is the devoured condition: " + condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
     });
 });
