68f4ed1e9fe8ec39570866187a857c22965c569d
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-09 13:17:23 -0400

    add partials to handlebars

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require('./controllers/burgers_controller.js');
+app.use(routes);
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
