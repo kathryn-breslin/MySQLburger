2107703115cdde1f1adfec9359def6c57e140ba6
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-09 10:26:29 -0400

    add api post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,5 +10,16 @@ router.get('/', function(req, res){
         };
         console.log(handlebarsObject);
         res.render('index', handlebarsObject);
-    })
-})
+    });
+});
+
+router.post('/api/burgers', function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+        //check index for correct name value
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
