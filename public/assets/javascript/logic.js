3b54d3534821e65b24458a33238bb0c0b1f42147
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-10 11:51:49 -0400

    burger button index.handlebars

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -1,19 +1,35 @@
 $(function() {
-    $('#submit').on('click',  function(event) {
-        event.preventDefault();
-        console.log("submit function")
-        var newBurger = {
-            burger_name: $('#burgerName').val(), 
-            devoured: 0
-        };
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+    console.log("submit function");
+    var newBurger = {
+      burger_name: $("#burgerName").val(),
+      devoured: 0
+    };
 
-        $.ajax('/api/burgers', {
-            type: "POST", 
-            data: newBurger
-        }).then(function() {
-            console.log("New burger has been created!" + newBurger);
-            // location.reload();
-        })
-
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("New burger has been created!" + newBurger);
+      location.reload();
     });
-})
+  });
+
+  $(".change-devour").on("submit", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newDevour");
+    console.log("Burger has been clicked")
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(function() {
+        console.log("Burger has been: " + newDevoured)
+        location.reload();
+    })
+  });
+});
